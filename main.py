3774f2e558e60dca4a6b73e1401ce0ecf2a91c4c
CharlieMallon <penguin47@hotmail.co.uk>
2021-01-29 16:43:48 +0000

    made output look nicer

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from scraper import JobScrape
 
 mon = JobScrape("monster")
 
+print("Working...")
+
 mon_results = mon.get_jobs("Telford", "uk", "python,developer")
 
-print(mon_results)
+for result in mon_results:
+    print(f"Job Title: {result['title']}")
+    print(f"Company: {result['company']}")
+    print(f"URL: {result['url']}")
+
+    if"description" in result:
+        print(f"Description: {result['description']}")
+    
+    print("----------")
+
+print(f"{len(mon_results)} jobs found")
+
+# print(mon_results)
 
 """ How to work out what is being returned"""
 
